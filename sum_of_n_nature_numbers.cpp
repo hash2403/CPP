8b1d21e1923296a737e7f47829feb060c3b14900
hash2403 <125856800+hash2403@users.noreply.github.com>
2023-06-24 11:43:45 +0530

    Create sum_of_n_nature_numbers.cpp

---
+++ sum_of_n_nature_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    float sum;
+    cout<<"enter n ";
+    cin>>n;
+    sum= (n*(n+1))/2;
+    cout<<"sum of n natural numbers "<<sum;
+    return 0;
+    
+    
+}
