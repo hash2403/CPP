d1c2e473c4eefba09a8a3cc343205716b7929e9d
hash2403 <125856800+hash2403@users.noreply.github.com>
2023-06-24 11:44:35 +0530

    Create area_of triangle.cpp

---
+++ area_of triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float h,b,area;
+    cout<<"Enter base and height ";
+    cin>>b>>h;
+    area= (h*b)/2;
+    cout<<"area of triangle is "<<area;
+    return 0;
+    
+    
+}
