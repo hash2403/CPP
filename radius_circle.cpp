89dfc8884442a6168e7575f16cc6d64da9e86c8c
hash2403 <125856800+hash2403@users.noreply.github.com>
2023-07-02 11:50:49 +0530

    Create radius_circle.cpp

---
+++ radius_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,area;
+    cout<<"Enter radius";
+    cin>>r;
+    area=(float)22/7*r*r;
+    cout<<"Area is"<<area<<endl;
+    return 0;
+    
+}
